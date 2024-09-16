248f03db6956781ab8f3e3070602e8d56fe3d1e0
kanishk <kanishkg830@gmail.com>
2024-09-16 10:08:00 +0530

    qr

--- pages/register/paytm.js
+++ pages/register/paytm.js
@@ -97,7 +97,7 @@ export default function PayTM() {
                 <div className='GooglePay__qr'>
                     <p className='GooglePay__qr--text'>Scan the QR to Pay</p>
                     <p style={{ fontSize: "1rem", marginTop: "0", marginBottom: ".5rem" }} className='GooglePay__qr--text'>Aditi Mohapatra</p>
-                    <img className='GooglePay__qr--image' src='/Images/Assets/aditiqr1.png' />
+                    <img className='GooglePay__qr--image' src='/Images/conf24/aditiqr.png' />
                     <div className='GooglePay__qr--TID'>
                         <input onChange={(e) => setTid(e.target.value)} type='text' placeholder='Transaction ID' required />
                     </div>
