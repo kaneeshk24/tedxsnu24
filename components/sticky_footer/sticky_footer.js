019f768fe76d4c98fb6cd546b96cd2840915a249
Rohit JG <rohitgokul817@gmail.com>
2024-09-18 00:36:45 +0530

    Added component for sticky ticket buy now footer

---
+++ components/sticky_footer/sticky_footer.js
@@ -0,0 +1,17 @@
+import './sticky_footer.scss'
+import Countdown from 'react-countdown';
+
+
+export default function StickyFooter() {
+
+    return(
+        <div className="StickyFooter">
+            <div className='StickyFooter__container fadeInUp-animation'>
+                 <p className="StickyFooter__container--text">Conference 24'</p>
+                 <Countdown className="StickyFooter__container--text" date={1729987250000} />
+                 <button className="StickyFooter__container--button">Get Ticket</button>
+            </div>        
+        </div>
+    )
+
+}
