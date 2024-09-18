767de5588f80cc9a60bf453cf6b0a52a6c41ec69
Kanishk gupta <110462331+kaneeshk24@users.noreply.github.com>
2024-09-18 10:46:00 +0530

    Update sticky_footer.js

--- components/sticky_footer/sticky_footer.js
+++ components/sticky_footer/sticky_footer.js
@@ -1,5 +1,6 @@
 import './sticky_footer.scss'
 import Countdown from 'react-countdown';
+import Link from 'next/link'
 
 
 export default function StickyFooter() {
@@ -9,7 +10,9 @@ export default function StickyFooter() {
             <div className='StickyFooter__container fadeInUp-animation'>
                  <p className="StickyFooter__container--text">Conference 24'</p>
                  <Countdown className="StickyFooter__container--text" date={1729987250000} />
+                <Link href='/register'>
                  <button className="StickyFooter__container--button">Get Ticket</button>
+                </Link>
             </div>        
         </div>
     )
